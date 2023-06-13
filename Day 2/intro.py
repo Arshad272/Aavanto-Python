f7ab3c68e46c9f383537ce3fbe0a6ca20ba809f5
Arshad272 <57592267+Arshad272@users.noreply.github.com>
2023-06-13 20:08:03 +0530

    day 2

---
+++ Day 2/intro.py
@@ -0,0 +1,16 @@
+# Syntax
+# Syntax is noting but set of rules
+
+# Comments
+# Comments are not executed by python interpreter
+# We can comment any line using '#' symbol
+
+# Variables
+# Loops
+# Conditional Statements
+# Data types
+# Data Structures
+# Functions
+# Classes
+# Objects
+# Modules
---
+++ Day 2/variables.py
@@ -0,0 +1,18 @@
+# Variables
+# Variables are containers which are used to store data
+
+a = 10
+# Variable_name = DATA 
+# print(a)
+
+a1b2 = 10.325
+print(a1b2)
+
+# Rules of Variables
+
+# 1. You cannot use special charecters in variable names except underscore ( _ )
+# 2. We cannot begin a variable name with number
+
+# Dynamically typed programming language
+
+
