f9fdc847b237138d4a0cf430907e20dba83dc8ac
Arshad272 <57592267+Arshad272@users.noreply.github.com>
2023-06-27 20:25:28 +0530

    day 13

---
+++ Day 13/for_loops.py
@@ -0,0 +1,43 @@
+# for loops
+
+# syntax:
+# for variable_name in/not in iterable : 
+#     code
+
+# lst = [4,69,8,65,589,59,5959]
+
+# for i in lst:
+#     print(i) 
+
+# s = "Python"
+# for i in s:
+#     print(i) 
+
+# d = {11: "a", 21: "b", 31: "c"}
+# for i in d :
+#     print(i) 
+
+# for i in range(len(lst)): 
+#     print(i, lst[i]) 
+
+# print(i) 
+
+# range() -> will return sequence of numbers
+# range(start_range, end_range, increment/decrement) -> will return a numbers 
+# mandatory parameter - ending_range
+# optional parameters - starting range and increment
+# range(1,5) -> 1 , 2 , 3 , 4 
+# range(1,5,2) -> 1 , 3 
+
+# for i in range(5, 20, 3):
+#     print(i) 
+
+# for i in range(50, 40, -1):
+#     print(i) 
+
+
+for i in range(10, 21):
+    print(i, end=" ")
+
+# print("hii", end="***") 
+# print("hello")
---
+++ Day 13/loops.py
@@ -0,0 +1,19 @@
+# Iteration
+# The process of executing a block of code again and again is called as iteration
+
+# 
+
+lst = [1,5,7,9,6,50,40]
+# print(lst[0])
+# print(lst[1])
+# print(lst[2])
+# print(lst[3])
+
+i = 0
+while i < len(lst):
+    print("Element", i+1 , "is", lst[i])
+    i += 1 
+# Element 1 is 1
+# Element 2 is 5
+# Element 3 is 7
+
